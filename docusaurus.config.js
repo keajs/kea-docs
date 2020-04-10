8a202bf8b19ff11879b58444acb5c7ea65b6f1c1
Marius Andra <marius.andra@gmail.com>
2020-04-10 13:32:12 +0200

    fix edit link

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -80,7 +80,7 @@ module.exports = {
             {
                 docs: {
                     sidebarPath: require.resolve('./sidebars.js'),
-                    editUrl: 'https://github.com/facebook/docusaurus/edit/master/website/',
+                    editUrl: 'https://github.com/keajs/kea-docs/edit/master/website/',
                 },
                 theme: {
                     customCss: require.resolve('./src/css/custom.css'),
