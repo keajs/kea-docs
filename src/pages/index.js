b49b571364aed7656e1472221dba669a432d1740
Marius Andra <marius.andra@gmail.com>
2020-04-12 21:12:02 +0200

    update frontpage boxes

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,28 +12,25 @@ const features = [
         imageUrl: 'img/undraw_docusaurus_mountain.svg',
         description: (
             <>
-                Docusaurus was designed from the ground up to be easily installed and used to get your website up and
-                running quickly.
+                Get up and running in minutes.
             </>
         ),
     },
     {
-        title: <>Focus on What Matters</>,
+        title: <>Production Scale</>,
         imageUrl: 'img/undraw_docusaurus_tree.svg',
         description: (
             <>
-                Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go ahead and move your docs into
-                the <code>docs</code> directory.
+                Kea has been used to build huge apps.
             </>
         ),
     },
     {
-        title: <>Powered by React</>,
+        title: <>Built on Strong Foundations</>,
         imageUrl: 'img/undraw_docusaurus_react.svg',
         description: (
             <>
-                Extend or customize your website layout by reusing React. Docusaurus can be extended while reusing the
-                same header and footer.
+                Redux. State Machines. Reactive Programming.
             </>
         ),
     },
