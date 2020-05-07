008fc5fd60ddb513a5df803435738e9de77e3715
Marius Andra <marius.andra@gmail.com>
2020-05-08 00:40:10 +0200

    add some testimonials

--- src/pages/index.js
+++ src/pages/index.js
@@ -47,6 +47,49 @@ const features = [
     },
 ]
 
+
+const QUOTES = [
+  {
+    thumbnail: 'https://avatars3.githubusercontent.com/u/1727427?s=460&u=68d3693009c289044285447b6ed667bdb08176d2&v=4',
+    name: 'Tim Glaser',
+    title: 'CTO at PostHog',
+    text: (
+      <>
+        I&apos;ve helped open source many projects at Facebook and every one
+        needed a website. They all had very similar constraints: the
+        documentation should be written in markdown and be deployed via GitHub
+        pages. I’m so glad that Docusaurus now exists so that I don’t have to
+        spend a week each time spinning up a new one.
+      </>
+    ),
+  },
+  {
+    thumbnail: 'https://avatars0.githubusercontent.com/u/727994?s=460&u=25f13f09babcef77247377af7f574280606609e7&v=4',
+    name: 'Madis Väin',
+    title: 'Software Craftsman at Namespace',
+    text: (
+      <>
+        Open source contributions to the React Native docs have skyrocketed
+        after our move to Docusaurus. The docs are now hosted on a small repo in
+        plain markdown, with none of the clutter that a typical static site
+        generator would require. Thanks Slash!
+      </>
+    ),
+  },
+  {
+    thumbnail: 'https://avatars1.githubusercontent.com/u/1869731?s=460&u=c7168af46965484cf33d024bcb751f6319b6d41c&v=4',
+    name: 'Scotty Bollinger',
+    title: 'Senior Front End Engineer at Elastic',
+    text: (
+      <>
+        We have 3-5 engineers working on the Kea codebase. We use TypeScript and have about 25 logic stores all connected via a parent app logic store.
+        Absolutely LOVE the library and, as a engineer new to Redux, I found this to be incredibly easy to get up to speed on and it seems to scale very well.
+      </>
+    ),
+  },
+];
+
+
 function Feature({ imageUrl, title, description }) {
     const imgUrl = useBaseUrl(imageUrl)
     return (
@@ -109,6 +152,33 @@ function Home() {
                         </div>
                     </section>
                 )}
+
+              <div className='homepage-testimonials'>
+                <div className="container">
+                  <div className="row">
+                    {QUOTES.map((quote) => (
+                      <div className="col" key={quote.name}>
+                        <div className="avatar avatar--vertical margin-bottom--sm">
+                          <img
+                            alt={quote.name}
+                            className="avatar__photo avatar__photo--xl"
+                            src={quote.thumbnail}
+                            style={{overflow: 'hidden'}}
+                          />
+                          <div className="avatar__intro padding-top--sm">
+                            <h4 className="avatar__name">{quote.name}</h4>
+                            <small className="avatar__subtitle">{quote.title}</small>
+                          </div>
+                        </div>
+                        <p className="text--center text--italic padding-horiz--md">
+                          {quote.text}
+                        </p>
+                      </div>
+                    ))}
+                  </div>
+                </div>
+              </div>
+
             </main>
         </Layout>
     )
