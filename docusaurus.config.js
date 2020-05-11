c92789d6c22f5f1b931a8cfafbf553344559fa27
Marius Andra <marius.andra@gmail.com>
2020-05-11 21:42:42 +0200

    stars in the footer

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,3 +1,5 @@
+const React = require('react')
+
 module.exports = {
     title: 'Kea 2.0',
     tagline: 'Production Ready State Management for React',
@@ -13,6 +15,7 @@ module.exports = {
                 alt: 'Kea Logo',
                 src: 'img/logo.svg',
             },
+            hideOnScroll: true,
             links: [
                 {
                     to: 'docs/introduction/what-is-kea',
@@ -58,7 +61,7 @@ module.exports = {
                     ],
                 },
                 {
-                    title: 'Please Support Kea',
+                    title: 'Support Kea',
                     items: [
                         {
                             label: 'OpenCollective',
@@ -70,8 +73,16 @@ module.exports = {
                         },
                     ],
                 },
+                {
+                    title: 'Give a star!',
+                    items: [
+                        {
+                            html: `<iframe src="https://ghbtns.com/github-btn.html?user=keajs&amp;repo=kea&amp;type=star&amp;count=true" frameborder="0" scrolling="0" width="100px" height="20px" style="vertical-align: sub;"></iframe>`,
+                        },
+                    ],
+                },
             ],
-            copyright: `Copyright © 2015-${new Date().getFullYear()} Marius Andra and other Kea contributors.`,
+            copyright: `<br/>Copyright © 2015-${new Date().getFullYear()} <a href='https://twitter.com/mariusandra' style='color:var(--ifm-footer-color);text-decoration: underline;'>Marius Andra</a> and other Kea contributors.`,
         },
     },
     presets: [
