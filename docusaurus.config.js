dcf560ee698d6dfc6812c5df7fcc71e8d897943e
Marius Andra <marius.andra@gmail.com>
2020-05-12 14:19:23 +0200

    add GA

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -81,6 +81,19 @@ module.exports = {
                         },
                         {
                             html: `<div style='opacity: 0.3;margin-top:3px;'>You're awesome!</div>`
+                        },
+                        {
+                            html: `<script>
+                              if (window.location.host === 'kea.js.org') {
+                                (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+                                (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+                                m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+                                })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+                        
+                                ga('create', 'UA-103436947-1', 'auto');
+                                ga('send', 'pageview');
+                              }
+                            </script>`
                         }
                     ],
                 },
