bfa388c66a4b24000e7ec25921c742a22f61388b
Marius Andra <marius.andra@gmail.com>
2020-06-18 12:28:38 +0200

    add posthog docusaurus plugin

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,6 +8,7 @@ module.exports = {
     favicon: 'img/favicon.ico',
     organizationName: 'keajs',
     projectName: 'kea',
+    plugins: ['posthog-docusaurus'],
     themeConfig: {
         navbar: {
             title: 'Kea 2.1',
@@ -80,28 +81,27 @@ module.exports = {
                             html: `<iframe src="https://ghbtns.com/github-btn.html?user=keajs&amp;repo=kea&amp;type=star&amp;count=true" frameborder="0" scrolling="0" width="100px" height="20px" style="margin-top:5px"></iframe>`,
                         },
                         {
-                            html: `<div style='opacity: 0.3;margin-top:3px;'>You're awesome!</div>`
+                            html: `<div style='opacity: 0.3;margin-top:3px;'>You're awesome!</div>`,
                         },
-                        {
-                            html: `<script>
-                              if (window.location.host === 'kea.js.org') {
-                                !function(t,e){var o,n,p,r;e.__SV||(window.posthog=e,e._i=[],e.init=function(i,s,a){function g(t,e){var o=e.split(".");2==o.length&&(t=t[o[0]],e=o[1]),t[e]=function(){t.push([e].concat(Array.prototype.slice.call(arguments,0)))}}(p=t.createElement("script")).type="text/javascript",p.async=!0,p.src=s.api_host+"/static/array.js",(r=t.getElementsByTagName("script")[0]).parentNode.insertBefore(p,r);var u=e;for(void 0!==a?u=e[a]=[]:a="posthog",u.people=u.people||[],u.toString=function(t){var e="posthog";return"posthog"!==a&&(e+="."+a),t||(e+=" (stub)"),e},u.people.toString=function(){return u.toString(1)+".people (stub)"},o="capture identify alias people.set people.set_once set_config register register_once unregister opt_out_capturing has_opted_out_capturing opt_in_capturing reset".split(" "),n=0;n<o.length;n++)g(u,o[n]);e._i.push([i,s,a])},e.__SV=1)}(document,window.posthog||[]);
-                                posthog.init('6pDQjCoo5w1uvLr9O-TtyrNT67SpnZQ8fLLpR5CqatQ', {api_host: 'https://app.posthog.com'})
-                                                                
-                                (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-                                (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-                                m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-                                })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-                                ga('create', 'UA-103436947-1', 'auto');
-                                ga('send', 'pageview');
-                              }
-                            </script>`.split('                              ').join('')
-                        }
                     ],
                 },
             ],
             copyright: `<br/>Copyright © 2015-${new Date().getFullYear()} <a href='https://twitter.com/mariusandra' style='color:var(--ifm-footer-color);text-decoration: underline;'>Marius Andra</a> and other Kea contributors.`,
         },
+        googleAnalytics: {
+            trackingID: 'UA-103436947-1',
+        },
+        posthog:
+            process.env.NODE_ENV === 'production'
+                ? {
+                      apiKey: '6pDQjCoo5w1uvLr9O-TtyrNT67SpnZQ8fLLpR5CqatQ',
+                      appUrl: 'https://app.posthog.com',
+                  }
+                : {
+                      apiKey: '8jVz0YZ2YPtP7eL1I5l5RQIp-WcuFeD3pZO8c0YDMx4',
+                      appUrl: 'http://localhost:8000',
+                      enableInDevelopment: true,
+                  },
     },
     presets: [
         [
