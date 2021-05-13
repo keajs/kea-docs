d80e5eca2514e4bacee6afca625ab32bd4b8a382
Marius Andra <marius.andra@gmail.com>
2021-05-13 23:52:51 +0200

    Update githubApi.js

--- src/components/intro/githubApi.js
+++ src/components/intro/githubApi.js
@@ -6,19 +6,27 @@ const usersCache = new Map()
 export const api = {
     getUser: async function (user) {
         if (!usersCache.has(user)) {
-            const url = `${API_URL}/users/${user}`
-            const response = await window.fetch(url)
-            const json = await response.json()
-            usersCache.set(user, response.status === 200 ? json : { error: json.message })
+            try {
+                const url = `${API_URL}/users/${user}`
+                const response = await window.fetch(url)
+                const json = await response.json()
+                usersCache.set(user, response.status === 200 ? json : { error: json.message })
+            } catch (error) {
+                return { error: error.message }
+            } 
         }
         return usersCache.get(user) || null
     },
     getRepositories: async function (user) {
         if (!repoCache.has(user)) {
-            const url = `${API_URL}/users/${user}/repos?per_page=250`
-            const response = await window.fetch(url)
-            const json = await response.json()
-            repoCache.set(user, response.status === 200 ? json : { error: json.message })
+            try {
+                const url = `${API_URL}/users/${user}/repos?per_page=250`
+                const response = await window.fetch(url)
+                const json = await response.json()
+                repoCache.set(user, response.status === 200 ? json : { error: json.message })
+            } catch (error) {
+                return { error: error.message }
+            }
         }
         return repoCache.get(user) || []
     },
