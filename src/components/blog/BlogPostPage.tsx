11ef7e03aeb991abcc3e1069b66131b857d0cd86
Marius Andra <marius.andra@gmail.com>
2022-05-08 01:30:20 +0200

    add squeak to blog

--- src/components/blog/BlogPostPage.tsx
+++ src/components/blog/BlogPostPage.tsx
@@ -4,60 +4,85 @@
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
  */
-// copied from: node_modules/@docusaurus/theme-classic/src/theme/BlogPostPage
 
 import React from 'react'
-import Layout from '@theme/Layout'
+import BlogLayout from '@theme/BlogLayout'
 import BlogPostItem from '@theme/BlogPostItem'
 import BlogPostPaginator from '@theme/BlogPostPaginator'
 import type { Props } from '@theme/BlogPostPage'
-import BlogSidebar from '@theme/BlogSidebar'
+import { PageMetadata, HtmlClassNameProvider, ThemeClassNames } from '@docusaurus/theme-common'
 import TOC from '@theme/TOC'
-import EditThisPage from '@theme/EditThisPage'
-import { ThemeClassNames } from '@docusaurus/theme-common'
-import { BlogForm } from '../forms/BlogForm'
+import clsx from 'clsx'
+import { Squeak } from '../squeak/Squeak'
 
-function BlogPostPage(props: Props): JSX.Element {
-    const { content: BlogPostContents, sidebar } = props
-    const { frontMatter, metadata } = BlogPostContents
-    const { title, description, nextItem, prevItem, editUrl } = metadata
-    const { hide_table_of_contents: hideTableOfContents } = frontMatter
-
-    return (
-        <Layout
-            title={title}
-            description={description}
-            wrapperClassName={ThemeClassNames.wrapper.blogPages}
-            pageClassName={ThemeClassNames.page.blogPostPage}
-        >
-            {BlogPostContents && (
-                <div className="container margin-vert--lg">
-                    <div className="row">
-                        <div className="col col--3">
-                            <BlogSidebar sidebar={sidebar} />
-                        </div>
-                        <main className="col col--7">
-                            <BlogPostItem frontMatter={frontMatter} metadata={metadata} isBlogPostPage>
-                                <BlogPostContents />
-                            </BlogPostItem>
-                            <div>{editUrl && <EditThisPage editUrl={editUrl} />}</div>
-                            <BlogForm />
-                            {(nextItem || prevItem) && (
-                                <div className="margin-vert--xl">
-                                    <BlogPostPaginator nextItem={nextItem} prevItem={prevItem} />
-                                </div>
-                            )}
-                        </main>
-                        {!hideTableOfContents && BlogPostContents.toc && (
-                            <div className="col col--2">
-                                <TOC toc={BlogPostContents.toc} />
-                            </div>
-                        )}
-                    </div>
-                </div>
-            )}
-        </Layout>
-    )
+function BlogPostPageMetadata(props: Props): JSX.Element {
+  const { content: BlogPostContents } = props
+  const { assets, metadata } = BlogPostContents
+  const { title, description, date, tags, authors, frontMatter } = metadata
+  const { keywords } = frontMatter
+  const image = assets.image ?? frontMatter.image
+  return (
+    <PageMetadata title={title} description={description} keywords={keywords} image={image}>
+      <meta property="og:type" content="article" />
+      <meta property="article:published_time" content={date} />
+      {/* TODO double check those article meta array syntaxes, see https://ogp.me/#array */}
+      {authors.some((author) => author.url) && (
+        <meta
+          property="article:author"
+          content={authors
+            .map((author) => author.url)
+            .filter(Boolean)
+            .join(',')}
+        />
+      )}
+      {tags.length > 0 && (
+        <meta property="article:tag" content={tags.map((tag) => tag.label).join(',')} />
+      )}
+    </PageMetadata>
+  )
 }
 
-export default BlogPostPage
+function BlogPostPageContent(props: Props): JSX.Element {
+  const { content: BlogPostContents, sidebar } = props
+  const { assets, metadata } = BlogPostContents
+  const { nextItem, prevItem, frontMatter } = metadata
+  const {
+    hide_table_of_contents: hideTableOfContents,
+    toc_min_heading_level: tocMinHeadingLevel,
+    toc_max_heading_level: tocMaxHeadingLevel,
+  } = frontMatter
+
+  return (
+    <BlogLayout
+      sidebar={sidebar}
+      toc={
+        !hideTableOfContents && BlogPostContents.toc && BlogPostContents.toc.length > 0 ? (
+          <TOC
+            toc={BlogPostContents.toc}
+            minHeadingLevel={tocMinHeadingLevel}
+            maxHeadingLevel={tocMaxHeadingLevel}
+          />
+        ) : undefined
+      }
+    >
+      <BlogPostItem frontMatter={frontMatter} assets={assets} metadata={metadata} isBlogPostPage>
+        <BlogPostContents />
+        <h2>Questions & Answers</h2>
+        <Squeak slug={metadata.permalink} />
+      </BlogPostItem>
+
+      {(nextItem || prevItem) && <BlogPostPaginator nextItem={nextItem} prevItem={prevItem} />}
+    </BlogLayout>
+  )
+}
+
+export default function BlogPostPage(props: Props): JSX.Element {
+  return (
+    <HtmlClassNameProvider
+      className={clsx(ThemeClassNames.wrapper.blogPages, ThemeClassNames.page.blogPostPage)}
+    >
+      <BlogPostPageMetadata {...props} />
+      <BlogPostPageContent {...props} />
+    </HtmlClassNameProvider>
+  )
+}
