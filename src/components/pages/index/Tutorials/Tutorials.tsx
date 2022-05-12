e42f245d82b885268a7b20320d9a05de9e7537e1
Marius Andra <marius.andra@gmail.com>
2022-05-12 11:35:30 +0200

    second coming soon

--- src/components/pages/index/Tutorials/Tutorials.tsx
+++ src/components/pages/index/Tutorials/Tutorials.tsx
@@ -11,29 +11,32 @@ const tutorials = [
   {
     name: 'Build a GitHub API client in 20 minutes',
     link: '/docs/tutorials/github',
+    comingSoon: true,
   },
   {
     name: 'Using keys with your logic',
     link: 'https://keajs.ck.page/80aecebec7',
+    comingSoon: true,
   },
   {
     name: 'Build an infinite list',
     link: 'https://keajs.ck.page/80aecebec7',
+    comingSoon: true,
   },
 ]
 export function Tutorials() {
   return (
     <section className="Tutorials">
       <h1>Kea Tutorials</h1>
-      {tutorials.map(({ name, link }, index) => (
+      {tutorials.map(({ name, link, comingSoon }, index) => (
         <div className="homepage-video" key={`${link}::${name}`}>
           {link.indexOf('https:') === 0 ? (
             <a href={link}>
-              <Thumbnail style={{ padding: 10 }} index={index} comingSoon />
+              <Thumbnail style={{ padding: 10 }} index={index} comingSoon={comingSoon} />
             </a>
           ) : (
             <Link to={link}>
-              <Thumbnail style={{ padding: 10 }} index={index} />
+              <Thumbnail style={{ padding: 10 }} index={index} comingSoon={comingSoon} />
             </Link>
           )}
           <h2>
